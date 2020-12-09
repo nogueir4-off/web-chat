04cd7bd9527efd75c6487b60873eb539e4fda7d7
burnednature <ecologicalteam@protonmail.com>
2020-12-09 18:07:22 -0300

    first commit

---
+++ public/chat.js
@@ -0,0 +1,68 @@
+const chat = document.getElementById('chat');
+const userInput = prompt('seu nome:') // document.querySelector('input[name=username]'); 
+const messageInput = document.querySelector('#chat input'); 
+const containerMessage = document.querySelector('.Messages');
+
+// const open = document.getElementById('sideBar');
+// const cont = document.querySelector('.container');
+// const painel = document.querySelector('.tableLeft');
+// const close = document.getElementById('sideBarClose');
+
+// close.addEventListener('click', () => {
+//     cont.classList.remove('show');
+//     painel.style.left = '-500px';
+// })
+
+// open.addEventListener('click', () => {
+//     cont.classList.add('show');
+//     painel.style.left = '0px';
+// })
+
+var socket = io('http://localhost:3000')
+
+function renderMessage(message) {
+	messageObj = document.createElement('div');
+
+	console.log(name)
+	if (message.author == userInput) {
+		messageObj.classList.add('Message')
+		messageObj.classList.add('Your')
+	} else {
+		messageObj.classList.add('Message')
+		messageObj.classList.add('Their')
+	}
+	
+	messageObj.innerHTML = `<p><span class='nick'>${message.author}</span> ${message.message}</p>`	
+	containerMessage.appendChild(messageObj);
+}
+
+// socket.on('previousMessages', function(messages) {
+// 	for (message of messages) {
+// 		renderMessage(message);
+// 	}
+// })
+
+socket.on('receivedMessage', function(message) {
+	renderMessage(message)
+})
+
+chat.addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	var author = userInput;
+	var message = messageInput.value;
+
+	if (author.length && message.length) {
+		var messageObject = {
+			author: author,
+			message: message,
+		};
+
+		renderMessage(messageObject);
+
+		socket.emit('sendMessage', messageObject);
+	}
+
+	containerMessage.scrollTop = containerMessage.scrollHeight - containerMessage.clientHeight;
+	messageInput.value = "";
+})
