84f0dab12687a4952fc67acaddc1cf8ecc847248
nogueir4-off <gabrielnogueiraoliveira@gmail.com>
2020-12-17 17:25:56 -0300

    mod14

--- public/chat.js
+++ public/chat.js
@@ -115,7 +115,7 @@ function closeName() {
 
 
 var socket = io('https://web-chat-app-v01.herokuapp.com/')
-// var socket = io('http://localhost:3000')
+// var socket = io('http://localhost:3000))
 
 var colorYourNow = 'purple';
 var colorTheirNow = 'purple1';
